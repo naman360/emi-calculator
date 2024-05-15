3ce6650e4c6f9d50f5f896e67a857d31e68f1222
naman360 <naman.geek@gmail.com>
2024-05-15 20:21:49 +0530

    feat: added emi and down payment logic

--- src/components/emi-calculator.tsx
+++ src/components/emi-calculator.tsx
@@ -1,18 +1,93 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { tenure as loanTenure } from "../util/constant";
-function EMICalculator() {
-  const [totalCost, setTotalCost] = useState(0);
-  const [interestRate, setInterestRate] = useState(0);
-  const [processingFee, setProcessingFee] = useState(0);
-  const [downPayment, setDownPayment] = useState(0);
-  const [loanAmount, setLoanAmount] = useState(0);
-  const [selectedTenure, setSelectedTenure] = useState(12);
-  const [emi, setEmi] = useState(0);
 
-  const defaultStyles = {
-    marginTop: "20px",
-    fontWeight: "bold",
+const defaultStyles = {
+  marginTop: "20px",
+  fontWeight: "bold",
+};
+
+type OtherCharges = "interest_rate" | "processing_fee";
+type RangeValues = "emi" | "down_payment";
+
+function EMICalculator() {
+  const [totalCost, setTotalCost] = useState<number>(0);
+  const [interestRate, setInterestRate] = useState<number>(10);
+  const [processingFee, setProcessingFee] = useState<number>(1);
+  const [downPayment, setDownPayment] = useState<number>(0);
+  const [loanAmount, setLoanAmount] = useState<number>(0);
+  const [selectedTenure, setSelectedTenure] = useState<number>(12);
+  const [emi, setEmi] = useState<number>(0);
+
+  const handleTotalCostChange = (value: string) => {
+    const parsedValue = Number(value);
+    if (parsedValue < 0) return;
+    setTotalCost(parsedValue);
   };
+
+  const handleExtraCharges = (value: string, type: OtherCharges) => {
+    const parsedValue = Number(value);
+
+    if (parsedValue < 0 || parsedValue > 100) return;
+    switch (type) {
+      case "interest_rate":
+        setInterestRate(parsedValue);
+        break;
+      case "processing_fee":
+        setProcessingFee(parsedValue);
+        break;
+    }
+  };
+
+  const calculateEmi = (dp: number, str?: string) => {
+    if (!totalCost) return 0;
+    // console.log(str);
+    const loanAmount = totalCost - dp;
+
+    const rateOfInterest = interestRate / 100;
+    const numOfYears = selectedTenure / 12;
+
+    console.log(loanAmount, rateOfInterest, selectedTenure / 12, dp, str);
+    const emiAmount =
+      (loanAmount * rateOfInterest * (1 + rateOfInterest) ** numOfYears) /
+      ((1 + rateOfInterest) ** numOfYears - 1);
+
+    return Number((emiAmount / 12).toFixed(0));
+  };
+
+  const calculateDownPayment = (emi: number) => {
+    if (!totalCost) return 0;
+
+    const downPaymentPercent = 100 - (emi / calculateEmi(0)) * 100;
+    return Number(((downPaymentPercent / 100) * totalCost).toFixed(0));
+  };
+
+  const updateEmi = (value: string) => {
+    if (!totalCost) return;
+    const parsedValue = Number(value);
+
+    if (parsedValue < 0) return;
+
+    setDownPayment(parsedValue);
+
+    const emiAmount = calculateEmi(parsedValue);
+    setEmi(emiAmount);
+  };
+
+  const updateDownPayment = (value: string) => {
+    if (!totalCost) return;
+    const parsedValue = Number(value);
+
+    if (parsedValue < 0) return;
+
+    setEmi(parsedValue);
+    const dp = calculateDownPayment(parsedValue);
+    setDownPayment(dp);
+  };
+
+  useEffect(() => {
+    const emi = calculateEmi(downPayment);
+    setEmi(emi);
+  }, [selectedTenure, totalCost]);
 
   return (
     <div
@@ -36,16 +111,35 @@ function EMICalculator() {
         </span>
 
         <span style={defaultStyles}>Total Cost of Asset</span>
-        <input type="number" />
+        <input
+          type="number"
+          value={totalCost}
+          onChange={(e) => handleTotalCostChange(e.target.value)}
+        />
 
         <span style={defaultStyles}>Interest Rate (in %)</span>
-        <input type="number" />
+        <input
+          type="number"
+          value={interestRate}
+          onChange={(e) => handleExtraCharges(e.target.value, "interest_rate")}
+        />
 
         <span style={defaultStyles}>Processing Fee (in %)</span>
-        <input type="number" />
+        <input
+          type="number"
+          value={processingFee}
+          onChange={(e) => handleExtraCharges(e.target.value, "processing_fee")}
+        />
 
         <span style={defaultStyles}>Down Payment</span>
-        <input type="range" name="" id="" />
+        <input
+          type="range"
+          min={0}
+          max={totalCost}
+          value={downPayment}
+          onChange={(e) => updateEmi(e.target.value)}
+          step={1}
+        />
         <span style={{ display: "flex", justifyContent: "space-between" }}>
           <span>0%</span>
           <span>{downPayment}</span>
@@ -53,11 +147,19 @@ function EMICalculator() {
         </span>
 
         <span style={defaultStyles}>EMI</span>
-        <input type="range" name="" id="" />
+        <input
+          type="range"
+          min={calculateEmi(totalCost, "hello")}
+          max={calculateEmi(0)}
+          value={emi}
+          onChange={(e) => updateDownPayment(e.target.value)}
+          name=""
+          id=""
+        />
         <span style={{ display: "flex", justifyContent: "space-between" }}>
-          <span>0%</span>
-          <span>{downPayment}</span>
-          <span>100%</span>
+          <span>{calculateEmi(totalCost)}</span>
+          <span>{emi}</span>
+          <span>{calculateEmi(0)}</span>
         </span>
 
         <span style={defaultStyles}>Tenure</span>
@@ -79,6 +181,7 @@ function EMICalculator() {
                 backgroundColor:
                   selectedTenure === tenure ? "#009ad6" : "rgb(226,226,226)",
               }}
+              onClick={() => setSelectedTenure(tenure)}
             >
               {tenure}
             </button>
